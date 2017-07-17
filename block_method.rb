4cdfd45f5d263b03044074fd3b7499a7c9f3b5bf
pbmuller <muller.98@osu.edu>
2017-07-17 18:11:54 -0400

    Delete block_method.rb

--- block_method.rb
+++
@@ -1,9 +0,0 @@
-def block_method
-	puts "This is the first line in block_method."
-	yield #This is the part of the method body that denotes this as a yeild method
-	puts "This statement is after the yield method"
-end
-
-block_method do 
-	puts "This is called from a block"
-end
